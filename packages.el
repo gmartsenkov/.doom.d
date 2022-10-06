166bc1e9ab1a2955bf794f8f24fc39e876f75e85
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-10-06 17:26:59 +0100

    Disable robe because it's injecting the company-backends with nonsense

--- packages.el
+++ packages.el
@@ -5,6 +5,7 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
+(package! robe :disable t)
 (package! polymode)
 (package! slim-mode)
 (package! gcmh)
